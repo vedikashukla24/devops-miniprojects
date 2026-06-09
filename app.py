53bf47409da4c99dbeafc0ff71dd7fcd950b885e
Vedika Shukla <vedikashukla1306@gmail.com>
2026-06-10 00:21:33 +0530

    Add dashbaord module

--- app.py
+++ app.py
@@ -4,6 +4,24 @@ def login():
     username = input("Enter Username: ")
     print(f"Welcome {username}")
 
+def dashboard():
+    print("\nDashboard")
+    print("1. View Profile")
+    print("2. Settings")
+    print("3. Logout")
+
+def main():
+    print("Git Workflow Simulator")
+    login()
+    dashboard()
+
+if __name__ == "__main__":
+    main()# Mini Project 02 - Git Workflow Simulator
+
+def login():
+    username = input("Enter Username: ")
+    print(f"Welcome {username}")
+
 def main():
     print("Git Workflow Simulator")
     login()
